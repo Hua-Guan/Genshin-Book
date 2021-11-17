6c3ab61a5201e3bf93275f606642687e9de518f0
HuaGuan <G18174078632@gmail.com>
2021-11-17 19:22:38 +0800

    更新

--- app/src/main/java/xyz/genshin/itismyduty/model/OverviewGridViewAdapter.kt
+++ app/src/main/java/xyz/genshin/itismyduty/model/OverviewGridViewAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import xyz.genshin.itismyduty.R
 
@@ -14,7 +15,7 @@ class OverviewGridViewAdapter(context: Context) : BaseAdapter() {
     private var context: Context = context
 
     override fun getCount(): Int {
-        return 1
+        return 5
     }
 
     override fun getItem(position: Int): Any {
@@ -25,17 +26,20 @@ class OverviewGridViewAdapter(context: Context) : BaseAdapter() {
         return 1
     }
 
-    @SuppressLint("InflateParams")
+    @SuppressLint("InflateParams", "SetTextI18n")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
         var view = convertView
         if (view == null){
             val holder = Holder()
-            view = LayoutInflater.from(context).inflate(R.layout.item_overview, null)
+            //必须传入parent, 不然gridview的高度会无效
+            view = LayoutInflater.from(context).inflate(R.layout.item_overview, parent, false)
             holder.image = view.findViewById(R.id.image)
+            holder.typeName =  view.findViewById(R.id.type_name)
             Glide.with(view)
                 .load("https://genshin.itismyduty.xyz/Role/role.jpg")
                 .into(holder.image)
-            convertView?.tag = holder
+            holder.typeName.text = "role"
+            view?.tag = holder
         }else{
 
 
@@ -46,7 +50,7 @@ class OverviewGridViewAdapter(context: Context) : BaseAdapter() {
     class Holder{
 
         lateinit var image: ImageView
-
+        lateinit var typeName: TextView
 
     }
 
--- app/src/main/java/xyz/genshin/itismyduty/view/MainActivity.kt
+++ app/src/main/java/xyz/genshin/itismyduty/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package xyz.genshin.itismyduty
+package xyz.genshin.itismyduty.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
