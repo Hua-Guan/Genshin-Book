6a25a8515c6f2daaf8a2643d67a08a804a07df0d
xvshunmiao <280527228@qq.com>
2021-12-10 18:54:57 +0800

    la

--- app/src/main/java/xyz/genshin/itismyduty/view/enemy/EnemyActivity.java
+++ app/src/main/java/xyz/genshin/itismyduty/view/enemy/EnemyActivity.java
@@ -3,14 +3,26 @@ package xyz.genshin.itismyduty.view.enemy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import xyz.genshin.itismyduty.R;
 
+/**
+ * @author xvshunmiao
+ */
 public class EnemyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enemy);
+        ImageView back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
